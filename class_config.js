978e70066b35c408106dc1f6386f2be80b60400f
vpietri-stack <v.pietri@esc-montpellier.com>
2026-02-02 08:06:41 +0800

    Update class_config.js

--- class_config.js
+++ class_config.js
@@ -29,7 +29,7 @@ const CLASS_CONFIG = {
     "周一": {
         "1810-1940": {
             students: ["Amy", "David", "Gavin", "Nick", "Sean", "Sophie"],
-            content: { book: "PU1", unit: "1", page: "16" }
+            content: { book: "PU1", unit: "2", page: "28" }
         }
     },
     "周四": {
@@ -41,13 +41,13 @@ const CLASS_CONFIG = {
     "周五": {
         "1900-2030": {
             students: ["Amy", "Colin", "Ethan", "Hollis", "Selena", "Sophia"],
-            content: { book: "PU1", unit: "1", page: "16" }
+            content: { book: "PU1", unit: "2", page: "28" }
         }
     },
     "周六": {
         "0900-1030": {
             students: ["Angel", "Eddy", "Dylan", "Mia", "YaoYao"],
-            content: { book: "PU1", unit: "1", page: "16" }
+            content: { book: "PU1", unit: "2", page: "28" }
         },
         "1040-1210": {
             students: ["Amy", "Annie", "Doris", "Harvey", "May", "Milk"],

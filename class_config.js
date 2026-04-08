e9b13214371f1a39d6e5e6c8f50423042c7cf9eb
vpietri-stack <v.pietri@esc-montpellier.com>
2026-04-08 16:17:27 +0800

    Update class_config.js

--- class_config.js
+++ class_config.js
@@ -134,7 +134,7 @@ const CLASS_CONFIG = {
         },
         "1810-1940": {
             students: ["Andy", "Iris", "Ivan", "Ruly", "Zozo"],
-            content: { book: "PU1", unit: "0", page: "5" }
+            content: { book: "PU1", unit: "0", page: "8" }
         }
     }
 };
